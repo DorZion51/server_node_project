5180d6f66d567105ca64d33827d9612b4fc591b6
DorZion51 <dor17.10@gmail.com>
2020-03-10 09:52:23 +0200

    v1.3

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express =require ('express');
-const app=express();
+const passport =require ('passport');
+const GoogleStartegy =require ('passport-google-oauth20').Strategy;
+const keys=require('./config/keys');
 
+const app=express();
+/*
 app.get('/',(req,res)=>{
     res.send({hi:'change'});
 });
+*/
 
+app.get(
+    '/auth/google',
+    passport.authenticate('google',{
+    scope:['profile','email']
+    })
+);
+
+passport.use(
+    new GoogleStartegy({
+    ClientID:keys.googleClientID,
+    ClientSecret:keys.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+    },
+    accessToken=>{
+    console.log(accessToken);
+    }
+    )
+);
 const PORT= process.env.PORT || 5000;
 
 app.listen(PORT);
